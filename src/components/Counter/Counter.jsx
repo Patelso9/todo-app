44f58e7c1235ca2fab76d13482d3251aecf99700
Princess <sonali.patel.pkg@gmail.com>
2022-10-11 11:43:04 -0500

    style & counter 2

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -9,16 +9,23 @@ class Counter extends Component {
   constructor(){
     super(); // Error 1: common to not include
 
-    this.state = { counter : 0 }
+    this.state = { 
+      counter : 0,
+      // secondCounter: 100 
+    }
 
     this.increment = this.increment.bind(this);
   }
 
   render () {
+    const style = {fontSize : "100px", padding : "30px 50px"}
   return (
     <div className="counter">
         <button onClick={this.increment}>+1</button>
-        <span className="count">{this.state.counter}</span>
+        <span className="count"
+          style={style}
+        >{this.state.counter}</span>
+        {/* <span className="count">{this.state.secondCounter}</span> */}
     </div>
   )
 }
@@ -27,7 +34,8 @@ class Counter extends Component {
   // console.log('increment')
   // this.state.counter++;  // Bad practice!
   this.setState({
-    counter: this.state.counter + 1
+    counter: this.state.counter + 1,
+    // secondCounter: this.state.secondCounter + 1
   });
 
  }
